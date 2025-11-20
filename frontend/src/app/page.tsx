458756117f946905596e95e6485222f9fb225868
Slipum <sabirovdanil7@gmail.com>
2025-11-20 22:15:36 +0300

    fix | add render all transactions

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Calendar } from '@/components';
+
 export default function Home() {
 	return <></>;
 }
---
+++ frontend/src/components/Calendar.tsx
@@ -0,0 +1,11 @@
+type CalendarProps = {};
+
+export function Calendar({}: CalendarProps) {
+	return (
+		<div>
+			<h1>Calendar Component</h1>
+		</div>
+	);
+}
+
+export default Calendar;
